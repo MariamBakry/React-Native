30011ef12fce26c326b1eb1bfdff279ac7815008
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-06-10 03:17:29 +0300

    lab1,2,3

---
+++ lab03/contexts/usersContextProvider.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import React ,{createContext,useEffect,useState}from "react";
+export const usersContext=createContext();
+const UsersContextProvider = (props) => {
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(res=>{
+            setUsers(res.data)
+        })
+
+   
+    },[])
+    
+    return (
+       <usersContext.Provider value={{users}}>
+        {props.children}
+       </usersContext.Provider>
+
+    );
+}
+
+
+export default UsersContextProvider;
